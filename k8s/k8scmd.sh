97a0295bad4218c955a184f7f8935f100608d14c
Prem Das <premkumardas2611@gmail.com>
2026-04-05 17:25:36 +0000

    Updated k8scmd.sh

--- k8s/k8scmd.sh
+++ k8s/k8scmd.sh
@@ -10,12 +10,16 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main
 sleep 30
 
 kubectl get pods -n ingress-nginx
-kubectl get pods -n ingress-nginx
+
 kubectl apply -f ingress.yaml -n e-commerce
-kubectl get svc -n e-commerce -o wide
-sleep 30
-kubectl get all -n e-commerce
 sleep 90
+
+kubectl get svc -n e-commerce -o wide
+sleep 15
+
+kubectl get all -n e-commerce
+sleep 30
+kubectl get endpoints -n ingress-nginx
 kubectl get ingress -n e-commerce
 
 
