81096de62df502039255cd35e1533d8bd0306949
Prem Das <premkumardas2611@gmail.com>
2026-04-05 12:19:47 +0000

    Updated k8scmd.sh

--- k8s/k8scmd.sh
+++ k8s/k8scmd.sh
@@ -1,18 +1,20 @@
 kubectl get nodes
 
 kubectl create namespace e-commerce
+sleep 5
 kubectl apply -f mongodb.yaml -n e-commerce
 kubectl apply -f backend.yaml -n e-commerce
 kubectl apply -f frontend.yaml -n e-commerce
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
 kubectl get pods -n ingress-nginx
-sleep 15
 kubectl get pods -n ingress-nginx
 kubectl apply -f ingress.yaml -n e-commerce
-sleep 15
+kubectl get svc -n e-commerce -o wide
+sleep 10
+kubectl get all -n e-commerce
+sleep 30
 kubectl get ingress -n e-commerce
 
 
 
-kubectl get svc -n e-commerce -o wide
